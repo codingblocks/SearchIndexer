3fc929a83b2d5114b293b8e62915f13f92a7af03
Russell Hammett <Kritner@gmail.com>
2019-04-09 11:24:20 -0400

    Some argument parsing stuff potentially?

---
+++ App/ArgumentParsing/ArgumentParsingTarget.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CommandLineParser.Arguments;
+
+namespace App
+{
+    public class ArgumentParsingTarget
+    {
+        private const string Running_Modes = "get;create;update;delete";
+
+        private const string Running_Modes_Helper =
+            "Specify the running mode of the application with \"" + Running_Modes + "\""; // can't use string interpolation on a const D:
+
+        [EnumeratedValueArgument(typeof(RunningMode), 'm', "mode", AllowedValues = Running_Modes, Optional = false, 
+            Description = Running_Modes_Helper)]
+        public RunningMode RunningMode;
+
+        [SwitchArgument('h', "help", true, Description = "Information on invoking app.")]
+        public bool Help;
+    }
+}
